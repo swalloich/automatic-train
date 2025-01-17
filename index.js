978821a4486f5ec459244d8f7df8a4a3e046301a
swalloich <nels.jac42@gmail.com>
2025-01-17 14:16:54 -0700

    update `DEFAULT_PORT`

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('./db').connect();
 
 app.use('/', require('./routes'));
 
-const DEFAULT_PORT = 3000;
+const DEFAULT_PORT = process.env.port || 8080;
 app.listen(process.env.port || DEFAULT_PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.port || DEFAULT_PORT}`);
 });
