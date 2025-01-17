c16db433a9fa5efba96b017e040688ec9e2a5d8c
swalloich <nels.jac42@gmail.com>
2025-01-17 13:44:14 -0700

    added routes index file

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const express = require('express');
-const app = express();
 require('./db').connect();
-
-const homeRoute = require('./routes/home');
-const contactsRoute = require('./routes/contacts');
-
-app.use('/', homeRoute);
-app.use('/contacts', contactsRoute);
+require('./routes');
 
 const DEFAULT_PORT = 3000;
 app.listen(process.env.port || DEFAULT_PORT, () => {
